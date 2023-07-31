5d7dd48c75af64f10bfcefa6a85329b189400d45
Rivaldi0097 <rivaldi1997@hotmail.com>
2023-07-31 21:48:42 +0800

    Update AboutMe.js

--- frontend/src/Components/AboutMe.js
+++ frontend/src/Components/AboutMe.js
@@ -29,13 +29,13 @@ function AboutMe(props) {
             className={isVisibility?"show":"hidden"}
         >
             <Box sx={{p:{sm:5, md:5}}}>
-                <Typography variant='h1' align='center'>Hello 👋, I am Rivaldi Kongres.</Typography>
+                <Typography variant='h2' align='center'>Hello 👋, I am Rivaldi Kongres.</Typography>
                 <br/>
-                <Typography variant='h4' align='center' sx={{fontSize:{xs:'2rem', sm:'2rem', md:'2rem'}}}>
+                <Typography variant='h5' align='center' sx={{fontSize:{xs:'2rem', sm:'2rem', md:'2rem'}}}>
                     An aspiring Full Stack Developer. This is a simple poftfolio just for you to get to know me better.
                 </Typography>
                 <br/>
-                <Typography variant='h4' align='center' sx={{fontSize:{xs:'2rem', sm:'2rem', md:'2rem'}}}>
+                <Typography variant='h5' align='center' sx={{fontSize:{xs:'2rem', sm:'2rem', md:'2rem'}}}>
                     I am currently based in Singapore and had just completed my study with Singapore Management Univesity (SMU).
                 </Typography>
             </Box>
